646e9d4b2630483a15ff8e703be299a490635472
Muhammad Bilal Taghi <78375305+Muhammad-Bilal-Taghi@users.noreply.github.com>
2022-02-21 22:02:04 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Resturant from './Components/Basic/Resturant'
+const App = () => {
+  return (
+    <div>
+         <Resturant/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Basic/menuApi.js
@@ -0,0 +1,59 @@
+const Menu = [
+{
+    id: 1,
+    image : "../Images/HalwaPori.jpg",
+    name : "HalwaPori",
+    category : "Breakfast",
+    Price : "Rs.40",
+    Description : "Our halwa is made of desi ghee",
+},
+
+{
+    id: 2,
+    image : "../Images/HalwaPori.jpg",
+    name : "ChanaPai",
+    category : "Breakfast",
+    Price : "Rs.120",
+    Description : "Speciality in ChanaPai",
+},
+
+{
+    id: 3,
+    image : "../Images/HalwaPori.jpg",
+    name : "Samosa",
+    category : "Lunch",
+    Price : "Rs.25",
+    Description : "Our halwa is made with desi ghee clarified butter, but I actually like using butter     because it gives a really good flavor, and of course is more easily available ",
+},
+
+{
+    id: 4,
+    image : "../Images/HalwaPori.jpg",
+    name : "Roll",
+    category : "Lunch",
+    Price : "Rs.25",
+    Description : "Lorem ipsum dolor sit amuet consecuter adisciping elit.",
+},
+
+{
+    id: 5,
+    image : "../Images/HalwaPori.jpg",
+    name : "Qeema Somosa",
+    category : "Lunch",
+    Price : "Rs.35",
+    Description : "Lorem ipsum dolor sit amuet consecuter adisciping elit.",
+},
+
+{
+    id: 6,
+    image : "../Images/HalwaPori.jpg",
+    name : "Burger Sada",
+    category : "Dinner",
+    Price : "Rs.50",
+    Description : "Lorem ipsum dolor sit amuet consecuter adisciping elit.",
+},
+
+
+];
+
+export default Menu;
